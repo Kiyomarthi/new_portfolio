c64e56cadc0af4397b37f283b5c38d6a66230a4e
kiumarsi <a.kiyomarthi@gmail.com>
2026-08-12 20:05:40 +0330

    feat: chat

--- app/app.vue
+++ app/app.vue
@@ -11,6 +11,14 @@ useHead({
     { name: "viewport", content: "width=device-width, initial-scale=1" },
     { key: "theme-color", name: "theme-color", content: color },
   ],
+
+  script: [
+    {
+      src = "https://cdn.pulse.is/livechat/loader.js",
+      "data-live-chat-id": "6a7c9de4f3bc2bbef10364ec",
+      async: true,
+    },
+  ],
 });
 
 useSeoMeta({
@@ -62,9 +70,3 @@ const [{ data: navigation }, { data: files }] = await Promise.all([
     </ClientOnly>
   </UApp>
 </template>
-
-<script
-  src="https://cdn.pulse.is/livechat/loader.js"
-  data-live-chat-id="6a7c9de4f3bc2bbef10364ec"
-  async
-></script>
