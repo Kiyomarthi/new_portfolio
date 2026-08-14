159ac536de97be50dfd91d8043ce843719d4d84d
kiumarsi <a.kiyomarthi@gmail.com>
2026-08-14 15:01:21 +0330

    fix: conflict with nuxt studio

---
+++ how HEADcontent.config.ts
@@ -0,0 +1,50 @@
+[33mcommit 6c4ceda4a3488a59b1ca1d44fe41292c96e1ee2c[m
+Author: kiumarsi <a.kiyomarthi@gmail.com>
+Date:   Wed Aug 12 19:03:44 2026 +0330
+
+    review
+
+[1mdiff --git a/content/projects/marcopro.yml b/content/projects/marcopro.yml[m
+[1mindex d692d1c..d4dca29 100644[m
+[1m--- a/content/projects/marcopro.yml[m
+[1m+++ b/content/projects/marcopro.yml[m
+[36m@@ -43,6 +43,7 @@[m [mimages:[m
+     alt: اسکرین‌شات‌های سایت مارکوپرو[m
+   - src: https://qr2tnegks1uabe3e.public.blob.vercel-storage.com/marcopro/demo.marcopro.co_%20%286%29.png[m
+     alt: اسکرین‌شات‌های سایت مارکوپرو[m
+[32m+[m
+ links:[m
+   - label: مشاهده سایت[m
+     to: https://demo.marcopro.co/[m
+[36m@@ -50,14 +51,19 @@[m [mlinks:[m
+     icon: i-lucide-external-link[m
+ startDate: 2025-10-27[m
+ tags:[m
+[32m+[m[32m  - Nuxt[m
+[32m+[m[32m  - Vue[m
+[32m+[m[32m  - Tailwind[m
+   - TypeScript[m
+   - Performance[m
+   - SEO[m
+   - Nuxt UI[m
+[32m+[m
+ technologies:[m
+   - category: frontend[m
+     items:[m
+       - Nuxt.js 4[m
+[32m+[m[32m      - Vue[m
+       - TypeScript[m
+       - Nuxt UI[m
+       - Pinia[m
+[36m@@ -71,5 +77,11 @@[m [mtechnologies:[m
+     items:[m
+       - "pwa"[m
+       - Leaflet[m
+[32m+[m[32m  - category: architecture[m
+[32m+[m[32m    items:[m
+[32m+[m[32m      - Atomic Design[m
+[32m+[m[32m      - Modular Architecture[m
+[32m+[m[32m      - Component-Based Architecture[m
+[32m+[m[32m      - Nuxt Layers[m
+ [m
+ url: https://demo.marcopro.co[m
