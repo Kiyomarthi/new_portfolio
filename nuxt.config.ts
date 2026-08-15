a3365c1276f13c492e42704e7300427ce69a946a
kiumarsi <a.kiyomarthi@gmail.com>
2026-08-15 22:11:00 +0330

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,119 +1,129 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/content',
-    '@vueuse/nuxt',
-    'nuxt-og-image',
-    'motion-v/nuxt',
-    'nuxt-studio',
-    '@nuxt/fonts'
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/content",
+    "@vueuse/nuxt",
+    "nuxt-og-image",
+    "motion-v/nuxt",
+    "nuxt-studio",
+    "@nuxt/fonts",
   ],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
   app: {
     head: {
       link: [
         {
-          rel: 'icon',
-          type: 'image/png',
-          href: '/favicon.ico'
-        }
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.ico",
+        },
       ],
       htmlAttrs: {
-        dir: 'rtl',
-        lang: 'fa'
-      }
-    }
+        dir: "rtl",
+        lang: "fa",
+      },
+    },
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   content: {
     experimental: {
-      sqliteConnector: 'native'
-    }
+      sqliteConnector: "native",
+    },
   },
 
-  compatibilityDate: '2026-06-30',
+  compatibilityDate: "2026-06-30",
 
   nitro: {
     prerender: {
-      routes: ['/'],
-      crawlLinks: true
-    }
+      routes: ["/"],
+      crawlLinks: true,
+    },
   },
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
   },
 
   fonts: {
-    provider: 'local',
+    provider: "local",
     families: [
       {
-        name: 'YekanBakh',
-        src: ['/fonts/yekan-bakh-normal-subset.woff2'],
-        weight: '400',
-        style: 'normal',
+        name: "YekanBakh",
+        src: ["/fonts/yekan-bakh-normal-subset.woff2"],
+        weight: "400",
+        style: "normal",
         preload: true,
         global: true,
-        display: 'swap',
-        unicodeRange: 'U+0600-06FF,U+06F0-06F9'
+        display: "swap",
+        unicodeRange: "U+0600-06FF,U+06F0-06F9",
       },
       {
-        name: 'YekanBakh',
-        src: ['/fonts/yekan-bakh-semibold-subset.woff2'],
-        weight: '500',
-        style: 'normal',
+        name: "YekanBakh",
+        src: ["/fonts/yekan-bakh-semibold-subset.woff2"],
+        weight: "500",
+        style: "normal",
         preload: true,
         global: true,
-        display: 'swap',
-        unicodeRange: 'U+0600-06FF,U+06F0-06F9'
+        display: "swap",
+        unicodeRange: "U+0600-06FF,U+06F0-06F9",
       },
       {
-        name: 'YekanBakh',
-        src: ['/fonts/yekan-bakh-bold-subset.woff2'],
-        weight: '700',
-        style: 'normal',
+        name: "YekanBakh",
+        src: ["/fonts/yekan-bakh-bold-subset.woff2"],
+        weight: "700",
+        style: "normal",
         preload: true,
         global: true,
-        display: 'swap',
-        unicodeRange: 'U+0600-06FF,U+06F0-06F9'
+        display: "swap",
+        unicodeRange: "U+0600-06FF,U+06F0-06F9",
       },
       {
-        name: 'grok',
-        src: ['/fonts/robeaugo.woff2'],
-        weight: '700',
-        style: 'normal',
+        name: "grok",
+        src: ["/fonts/robeaugo.woff2"],
+        weight: "700",
+        style: "normal",
         preload: true,
         global: true,
-        display: 'swap',
-        unicodeRange: 'U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+2000-206F'
-      }
-    ]
+        display: "swap",
+        unicodeRange: "U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+2000-206F",
+      },
+    ],
+  },
+
+  image: {
+    provider: "ipx",
+    domains: ["qr2tnegks1uabe3e.public.blob.vercel-storage.com"],
+    ipx: {
+      maxAge: 60 * 60 * 24 * 365,
+    },
+    format: ["avif", "webp"],
+    quality: 75,
   },
 
   ogImage: {
-    zeroRuntime: true
+    zeroRuntime: true,
   },
 
   studio: {
     repository: {
-      provider: 'github',
-      owner: 'Kiyomarthi',
-      repo: 'new_portfolio',
-      branch: 'main'
-    }
-  }
-})
+      provider: "github",
+      owner: "Kiyomarthi",
+      repo: "new_portfolio",
+      branch: "main",
+    },
+  },
+});
